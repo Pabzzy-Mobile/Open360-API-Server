1ce1c93636e04dc7867c3cdac6eda6bd4934adae
Pedro Pires <plpires99@gmail.com>
2020-11-14 16:57:09 +0000

    API server now relays messages between services

--- main.js
+++ main.js
@@ -1,1 +1,156 @@
-console.log("HELLO!")
+const http = require('http').createServer();
+const redis = require('redis');
+// Create the database client
+let RedisClient = redis.createClient({
+    host: 'open360-redis-sock',
+    port: 6379
+});
+
+// Listen for any database errors
+RedisClient.on("error", function(error) {
+    console.error(error);
+});
+
+// Require our core modules
+const {Util} = require("./core/");
+
+// Tell the server what port it should use. 4000 is for testing purposes
+const PORT = parseInt(process.env.API_PORT) || 4000;
+
+// Start the socket server
+const {Server, Socket} = require('socket.io');
+const io = new Server(http, {
+    // Allow cross origin requests
+    cors: {
+        // The `*` is used as the wildcard here.
+        origin: "*",
+        // Set the other options according to your needs.
+        // The docs are here:
+        // https://www.npmjs.com/package/cors#configuration-options
+        methods: ["GET", "POST"],
+        allowedHeaders: ["content-type"]
+    }
+});
+
+// Require some data over the connections start
+io.use((socket, next) => {
+    let handshakeData = socket.request;
+
+    // Register the socket to the database along with the room the socket is trying to join
+    RedisClient.set(socket.id + "room", 'universal');
+    let name = handshakeData._query['name'];
+    if (name == ""){
+        RedisClient.set(socket.id + "name", false);
+        RedisClient.set(socket.id + "colour", false);
+    } else {
+        RedisClient.set(socket.id + "name", name);
+        RedisClient.set(socket.id + "colour", Util.randomColour());
+    }
+
+    next();
+});
+
+// On socket connection
+io.on('connection', (socket) => {
+    //console.log("New connection on: " + socket.id);
+    //log(socket, "connected");
+    // Get the room the socket is supposed to be in and add it to the room
+    RedisClient.get(socket.id + "room", (err, reply) => {
+        socket.join(reply);
+    });
+
+    socket.on("log", (data) => {
+        log(socket, data.log, data.type);
+    });
+
+    socket.on("api-message", (data) => {
+        switch (data.type) {
+            case "question":
+                log(socket, "Message relayed:" + JSON.stringify(data), "info");
+                socket.broadcast.emit(data.target, data);
+                break;
+            case "message":
+                log(socket, "Message relayed:" + JSON.stringify(data), "info");
+                socket.broadcast.emit(data.target, data);
+                break;
+        }
+    });
+
+    // On disconnect delete the socket from the database
+    socket.on('disconnect', () => {
+        //log(socket, "disconnected");
+    });
+});
+
+http.listen(PORT, function () {
+    console.log("Listening on *:", PORT);
+});
+
+/**
+ * Logs a message from the socket given
+ * @param {Socket} socket
+ * @param {string} message
+ * @param {string} [type] - "error", "warn", "info" or "log" type beat
+ */
+function log(socket, message, type){
+    // Retrieve the name and colour of the service from the database
+    let promises = [
+        getName(socket),
+        getColour(socket)
+    ]
+    Promise.all(promises)
+        .then((data) => {
+            // Define different types of styles
+            let style = [
+                "color: #fff",
+                "background-color: #" + data[1] + "",
+                "padding: 2px 4px",
+                "border-radius: 2px"
+            ].join(';');
+            //
+            let time = Date.now().toLocaleString("en-UK");
+            // Log the message according to the type
+            switch (type){
+                case "error":
+                    console.error(`[${time}][%c${data[0]}][ERROR]`, style, message);
+                    break;
+                case "info":
+                    console.info(`[${time}][%c${data[0]}][INFO]`, style, message);
+                    break;
+                case "warn":
+                    console.warn(`[${time}][%c${data[0]}][WARN]`, style, message);
+                    break;
+                default:
+                    // If no type was passed log normally
+                    console.log(`[${time}][%c${data[0]}]`, style, message);
+            }
+        });
+}
+
+/**
+ * Gets the name of the socket from the database
+ * @param {Socket} socket
+ * @return {Promise<string>}
+ */
+function getName(socket){
+    return new Promise(resolve => {
+        // Get the name of the socket
+        RedisClient.get(socket.id + "name", (err, name) => {
+            resolve(name);
+        });
+    });
+}
+
+/**
+ * Gets the colour of the socket from the database
+ * @param {Socket} socket
+ * @return {Promise<string>}
+ */
+function getColour(socket){
+    return new Promise(resolve => {
+        // Get the colour of the socket
+        RedisClient.get(socket.id + "colour", (err, colour) => {
+            resolve(colour);
+        });
+    });
+}
